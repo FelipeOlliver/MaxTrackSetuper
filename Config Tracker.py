6db675e085cacd3ba999a30cb826aa6177fb7114
Felipe Oliveira <38664849+FelipeOlliver@users.noreply.github.com>
2018-07-29 02:30:13 -0300

    Add files via upload

---
+++ Config Tracker.py
@@ -0,0 +1,103 @@
+import pyautogui
+import time
+#pyautogui.position()
+#MAXTRACKSETUP
+pyautogui.click(424, 745)
+time.sleep(30)
+#MTC
+pyautogui.click(523, 139)
+#STANDARD
+pyautogui.click(474, 215)
+#DOOR
+pyautogui.click(487, 61)
+#REFRESHLIST
+pyautogui.click(469, 68)
+#BAUDRATE
+pyautogui.click(683, 51)
+time.sleep(2)
+pyautogui.click(672, 66)
+time.sleep(1)
+#LER ID
+pyautogui.click(545, 723)
+time.sleep(3)
+#LER SETUP
+pyautogui.click(640, 721)
+time.sleep(3)
+#GENERAL
+pyautogui.click(683, 237)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+#DEFAULTMODE
+pyautogui.click(634, 605)
+time.sleep(5)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+#SENDCONFIG
+pyautogui.click(587, 564)
+time.sleep(1)
+pyautogui.typewrite('vivo')
+#pyautogui.typewrite('claro')
+pyautogui.hotkey('down')
+time.sleep(1)
+pyautogui.hotkey('enter')
+time.sleep(22)
+#LER ID
+pyautogui.click(545, 723)
+time.sleep(5)
+#LER SETUP
+pyautogui.click(640, 721)
+time.sleep(4)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+#SECURITY
+pyautogui.click(694, 603)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+time.sleep(1)
+pyautogui.click(608, 463)
+time.sleep(1)
+pyautogui.typewrite('5353')
+#SENDCOMANDS
+pyautogui.click(761, 716)
+time.sleep(4)
+#EXECUTE_AND_RESTORE_BACKUP
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.click(503, 374)
+time.sleep(3)
+pyautogui.click(503, 374)
+time.sleep(3)
+pyautogui.click(517, 348)
+#DONE
+
+
+
---
+++ Config Tracker2.py
@@ -0,0 +1,68 @@
+import pyautogui
+import time
+
+
+
+#LER ID
+pyautogui.click(545, 723)
+time.sleep(3)
+#LER SETUP
+pyautogui.click(640, 721)
+time.sleep(3)
+#DEFAULTMODE
+pyautogui.click(628, 315)
+time.sleep(4)
+#SENDCONFIG
+pyautogui.click(590, 465)
+time.sleep(1)
+#pyautogui.typewrite('vivo')
+pyautogui.typewrite('claro')
+pyautogui.hotkey('down')
+time.sleep(1)
+pyautogui.hotkey('enter')
+time.sleep(22.5)
+#LER ID
+pyautogui.click(545, 723)
+time.sleep(5)
+#LER SETUP
+pyautogui.click(640, 721)
+time.sleep(4)
+#SECURITY
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+pyautogui.vscroll(-50)
+time.sleep(1)
+pyautogui.click(605, 463)
+time.sleep(1)
+pyautogui.typewrite('5353')
+#SENDCOMANDS
+pyautogui.click(761, 716)
+time.sleep(4)
+#EXECUTE_AND_RESTORE_BACKUP
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.vscroll(50)
+pyautogui.click(503, 374)
+time.sleep(3)
+pyautogui.click(503, 374)
+time.sleep(3)
+pyautogui.click(517, 348)
+#DONE
